5c389318a073a193ebff8e312254c71bb91bf35f
Mostafa Saleh <mostafa.saleh@bugzy.net>
2019-10-08 18:37:31 +0200

    Initial Commit

---
+++ Quran/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quran
+//
+//  Created by Mostafa Saleh on 10/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
