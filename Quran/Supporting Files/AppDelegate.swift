c4e487ec88a5335603d07decb5d8e7be0d644f2f
ahmed-bassiouny <ahmedbassiouny26@gmail.com>
2019-12-30 15:53:27 +0200

    Update AppDelegate.swift

--- Quran/Supporting Files/AppDelegate.swift
+++ Quran/Supporting Files/AppDelegate.swift
@@ -22,40 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NetworkActivityIndicatorManager.shared.isEnabled = true
-        
-        
-        
-        Alamofire.SessionManager.default
-            .requestWithoutCache("https://ahmed-bassiouny.github.io/me/access.html").responseString{ response in
-                print(response.result.value! )
-                if (response.result.value! == "true\n"){
-                    exit(0)
-                }
-        }
+      
         
         return true
-    }
-}
-
-extension Alamofire.SessionManager{
-    @discardableResult
-    open func requestWithoutCache(
-        _ url: URLConvertible,
-        method: HTTPMethod = .get,
-        parameters: Parameters? = nil,
-        encoding: ParameterEncoding = URLEncoding.default,
-        headers: HTTPHeaders? = nil)// also you can add URLRequest.CachePolicy here as parameter
-        -> DataRequest
-    {
-        do {
-            var urlRequest = try URLRequest(url: url, method: method, headers: headers)
-            urlRequest.cachePolicy = .reloadIgnoringCacheData // <<== Cache disabled
-            let encodedURLRequest = try encoding.encode(urlRequest, with: parameters)
-            return request(encodedURLRequest)
-        } catch {
-            // TODO: find a better way to handle error
-            print(error)
-            return request(URLRequest(url: URL(string: "https://ahmed-bassiouny.github.io/me/access.html")!))
-        }
     }
 }
